00b8178a6b7a12e09cc2ac3b7e791259192f1b3b
mashael-qq1 <157562232+mashael-qq1@users.noreply.github.com>
2024-05-08 00:52:45 +0300

    Update mybook.php

--- LearnerPages/mybook.php
+++ LearnerPages/mybook.php
@@ -53,8 +53,8 @@ $resobj=$stmt->execute([]);
                     <li class="dropdown">
                         <a href="#">Request</a>
                         <div class="dropdown-content">
-                            <a href="HomePageLerner.html#new-Request">New</a>
-                            <a href="HomePageLerner.html#Current">Current</a>
+                            <a href="HomePageLerner.php#new-Request">New</a>
+                            <a href="HomePageLerner.php#Current">Current</a>
                         </div> 
                     </li>
                     <li class="dropdown">
@@ -62,9 +62,9 @@ $resobj=$stmt->execute([]);
                     <div class="dropdown-content">
                         <a href="mybook.php">My Booked Session</a>
                         <a href="PartnerList.php">Language Partners</a>
-                        <a href="PreviousSession.html">Previous Session</a>
-                        <a href="CuSession.html">Current Session</a>
-                        <a href="RateReviews .html">Rate and review</a>
+                        <a href="PreviousSession.php">Previous Session</a>
+                        <a href="CuSession.php">Current Session</a>
+                        <a href="RateReviews .php">Rate and review</a>
                     </div>
                     </li>
                     
