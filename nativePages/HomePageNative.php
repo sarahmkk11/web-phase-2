8703a265d728404aa7c75278805c79a235c621a4
ahad <439200907@student.ksu.edu.sa>
2024-05-07 10:50:26 +0300

    add messages

--- nativePages/HomePageNative.php
+++ nativePages/HomePageNative.php
@@ -1,10 +1,85 @@
 <?php 
+   echo "
+   <script src='https://code.jquery.com/jquery-3.7.1.slim.min.js' integrity='sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8=' crossorigin='anonymous'></script>
+   <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js' integrity='sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T' crossorigin='anonymous'></script>
+    ";
     session_start();
 
     if(!isset($_SESSION['userName']))
       header("Location:../homePage/loginNF.php?error=Please Sign In again!");
 
     else{
+
+
+
+      
+      $dsn = 'mysql:dbname=projectdb;host=127.0.0.1;port=3306;';
+      $user = 'root';
+      $password = '';
+      $db = new PDO($dsn, $user, $password);
+      
+if( isset($_REQUEST['allmessages'])){
+  echo "<script type='text/javascript'>
+  $(document).ready(function(){
+  $('#messageModel').modal('show');
+  });
+  </script>";
+}
+
+
+
+
+
+
+
+if( isset($_REQUEST['messageidforlarner'])){
+
+  if( isset($_REQUEST['msgcontent'])){
+
+      $ins_query="insert into chate(language_partners_email,language_learners_email,msgcontent,datesend,msgtype) values (?,?,?,?,?);";
+      $stmt=$db->prepare($ins_query);
+      $parttener_email=$_SESSION['email'];
+     $datesend=date('Y-m-d H:i:s');
+        $learner_email=$_REQUEST["lernar_email_messag"];   
+    
+     
+      $msgcontent=$_REQUEST["msgcontent"];
+      @$res =$stmt->execute([$parttener_email,$learner_email,$msgcontent,$datesend,"recive"]);
+      echo "<script type='text/javascript'>
+      $(document).ready(function(){
+      $('#messagelearnerModel').modal('show');
+      });
+      </script>";
+     
+    }
+  
+
+  $select_query_chat="select * from chate where language_partners_email='{$_SESSION['email']}' AND language_learners_email='{$_REQUEST['messageidforlarner']}'";
+  $stmt_chat=$db->prepare($select_query_chat);
+  $resobj=$stmt_chat->execute([]);
+
+  $select_query_leardata="select * from language_learners where email='{$_REQUEST['messageidforlarner']}'";
+  $stmt_learner=$db->prepare($select_query_leardata);
+  $resobj_learner=$stmt_learner->execute([]);
+  if($stmt_learner->rowCount()>0)
+  {
+    $row_learner_data = $stmt_learner->fetch(PDO::FETCH_ASSOC);
+  
+  }
+   $fullname=$row_learner_data["first_name"]." ".$row_learner_data["last_name"] ."." ;
+   $imageofpartner=$row_learner_data['photo'];
+   $lernar_email_messag=$row_learner_data["email"];
+
+   echo "<script type='text/javascript'>
+   $(document).ready(function(){
+   $('#messagelearnerModel').modal('show');
+   });
+   </script>";
+
+}   
+
+
+
 
       ?>
 
@@ -27,6 +102,8 @@
     <link rel="stylesheet" href="../LearnerPages/HomePageLerner.css">
     <link rel="stylesheet" href="../LearnerPages/styleRequest.css">
     <link rel="stylesheet" href="HomePageNative.css">
+    <link rel="stylesheet" href="style.css">
+
 
 </head>
 
@@ -42,6 +119,7 @@
         <nav class="mainMenu">
             <ul>
                 <li><a href="#">Home</a></li>
+                <li><a href="HomePageNative.php?allmessages">messages</a></li>
 
                 <li class="dropdown">
                     <a href="#">Requests</a>
@@ -152,6 +230,185 @@
             <button class="Delete-button">Reject</button>
         </div>
       </section>
+
+
+      <br>
+
+<div id="messagelearnerModel" class="modal">
+<div class="modal-content" style="margin-top: 10%">
+  <span class="close">&times;</span>
+  
+  <div class="modal-header">
+    <div class="model_header_title">
+      <div class="model_header_t1">
+      <?php echo $fullname;?>
+
+      </div>
+    </div>
+  </div>
+  <div class="modal-body">
+    <div class="body_content">
+
+
+
+
+    <?php 
+  if( isset($_REQUEST['messageidforlarner'])){
+  if($stmt_chat->rowCount()>0)
+       {
+         while ($row_chat = $stmt_chat->fetch(PDO::FETCH_ASSOC)){
+          // var_dump ($row_chat);
+         
+  
+  if($row_chat['msgtype']=="recive"){
+    if($_SESSION['photo']==null)
+    $img="4325945.png";
+  else
+  $img=$_SESSION['photo'];
+$myname=$_SESSION['userName'] ." ".$_SESSION['lastName'];
+  }else{
+    if($imageofpartner==null)
+    $img="4325945.png";
+  else
+  $img=$imageofpartner;
+  $myname=$fullname;
+  }
+   
+   echo "
+      <div class='body_content_day'>
+        <div class='body_item'>{$row_chat['datesend']}</div>
+      </div>
+      <div class='model_content_side'>
+        <img
+          src=../homePage/uploads/$img
+          alt=''
+          width='50
+          height='50'
+          style='border-radius: 50%'>
+        <div class='model_content_hi'>$myname</div>
+      </div>
+      <div class='model_send'>
+        <div>{$row_chat['msgcontent']}</div>
+        <!-- <div>send</div> -->
+      </div>";
+       }
+      }else
+      {
+        echo " <h1 style='text-align: center;'> start message</h1>";
+      }
+  }
+      ?>
+
+
+      <div class="model_textarea">
+      <form method="Post" action ='<?php echo "HomePageNative.php?messageidforlarner=$lernar_email_messag";?>' >
+      <input type="text" value="<?php echo $lernar_email_messag; ?>" name="lernar_email_messag" hidden>
+      <textarea name="msgcontent" id="0" cols="30" rows="2" placeholder="your message"  required> </textarea>
+      <input type="submit" value="Send Message" style="margin-top:3px;">
+   
+</form>
+      </div>
+      
+    </div>
+  </div>
+</div>
+
+</div>
+
+
+
+
+
+
+
+
+<div id="messageModel" class="modal">
+<div class="modal-content" style="margin-top: 10%">
+  <span class="close">&times;</span>
+
+
+  <div class="modal-header">
+    <div class="model_header_title">
+      <div class="model_header_t1">
+
+        <?php echo $_SESSION['userName'] ." ".$_SESSION['lastName'];?>.</div>
+    </div>
+  </div>
+  <div class="modal-body">
+    <div class="body_content">
+      <?php 
+          $par_email=$_SESSION['email'];
+          $select_querychat="select * from chate where language_partners_email='$par_email' GROUP BY language_learners_email DESC;";
+          $stmtchat=$db->prepare($select_querychat);
+          $resobj=$stmtchat->execute([]);
+if($stmtchat->rowCount()>0)
+{
+
+
+while ($row = $stmtchat->fetch(PDO::FETCH_ASSOC)){
+   $select_query_learner="select * from language_learners where email='{$row['language_learners_email']}'";
+  $stmtlearner=$db->prepare($select_query_learner);
+  $resobj=$stmtlearner->execute([]);
+   if($stmtlearner->rowCount()>0)
+  $row_learner = $stmtlearner->fetch(PDO::FETCH_ASSOC);
+  if($row_learner['photo']==null)
+  $img="4325945.png";
+else
+$img=$row_learner['photo'];
+ 
+      echo"
+      <div class='body_content_day'>
+        <div class='body_item'>{$row['datesend']}</div>
+      </div>
+      <div class='model_content_side'>
+     <a href='HomePageNative.php?messageidforlarner={$row_learner['email']}'>
+      <img
+       src=../homePage/uploads/$img
+       alt=''
+       width='50'
+       height='50'
+       style='border-radius: 50%'>
+       </a>
+        <div class='model_content_hi' >{$row_learner['first_name']} {$row_learner['last_name']}</div>
+      </div>
+      <div class='model_send'>
+        <div style='font-weight: bold;'> {$row['msgcontent']} </div>
+      </div>
+      <hr width='90%'>
+  
+      </div>";
+}
+  }else{
+      echo " <h1 style='text-align: center;'> no chat yet </h1>";
+  }
+         ?>
+      
+    </div>
+  </div>
+</div>
+</div>
+
+
+<!-- Include Font Awesome -->
+<script>
+
+var modal = document.getElementById("messagelearnerModel");
+var modal2 = document.getElementById("messageModel");
+
+
+
+var span = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("close")[1];
+
+
+
+span.onclick = function () {
+modal.style.display = "none";
+};
+span2.onclick = function () {
+modal2.style.display = "none";
+};
+</script>
 
 
 
