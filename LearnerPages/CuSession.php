6d76d14726c35787c77b4c2683bafb4b9c83e074
mashael-qq1 <157562232+mashael-qq1@users.noreply.github.com>
2024-05-08 05:59:40 +0300

    Update CuSession.php

--- LearnerPages/CuSession.php
+++ LearnerPages/CuSession.php
@@ -48,3 +48,109 @@ if ($result->num_rows > 0) {
 }
 $conn->close();
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="hi language speaker! what do you want to do?">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="Learn, Languages, education">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="CuSession.css">
+    <style>
+        .btn-delete {
+            background-color: #f44336;
+            color: white;
+            border: none;
+            padding: 10px 20px;
+            border-radius: 5px;
+            cursor: pointer;
+            margin-right: 10px;
+        }
+        .btn-delete:hover {
+            background-color: #d32f2f;
+        }
+        .btn-continue {
+            background-color: #4caf50;
+            color: white;
+            border: none;
+            padding: 10px 20px;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+        .btn-continue:hover {
+            background-color: #388e3c;
+        }
+        .progress-bar {
+            background-color: #4caf50; /* Green */
+            height: 5px;
+            margin-top: 5px;
+            position: relative;
+        }
+        .progress-result {
+            position: absolute;
+            top: 100%;
+            left: 0;
+            width: 100%;
+            text-align: center;
+            font-size: 14px;
+            color: #333;
+            margin-top: 5px;
+        }
+    </style>
+    <title>View Current Sessions</title>
+</head>
+<body>
+    <header>
+    <div class="logo">
+            <img src="../homePage/logo bule.jpeg">
+            <h1>Talk Tandem</h1>
+        </div>
+
+        <nav class="mainMenu">
+            <ul>
+                <li><a href="HomePageLerner.php">Home</a></li>
+
+                <li class="dropdown">
+                    <a href="HomePageLerner.php#Request">Request</a>
+                    <div class="dropdown-content">
+                        <a href="HomePageLerner.php#new-Request">New</a>
+                        <a href="HomePageLerner.php#Current">Current</a>
+                    </div> 
+                </li>
+                <li class="dropdown">
+                <a href="#">View</a>
+                <div class="dropdown-content">
+                    <a href="PartnerList.php">Language Partners</a>
+                    <a href="PreviousSession.php">Previous Session</a>
+                    <a href="CuSession.php">Current Session</a>
+                    <a href="RateReviews.php">Rate and review</a>
+                </div>
+                </li>
+                
+                <li><a href="LearnerProfile.php">Manage Profile</a></li>
+             
+                <li><a href="../homePage/HomePage.php">Sign Out</a></li>
+            </ul> 
+        </nav>
+    </header>
+
+    <div class="container">
+    
+
+    </div>
+
+    <footer>
+    <div class="social-icons">
+      <a href="https://www.facebook.com/talktandem" class="icon facebook"><i class="fab fa-facebook"></i></a>
+      <a href="https://www.twitter.com/talktandem" class="icon twitter"><i class="fab fa-twitter"></i></a>
+      <a href="https://www.instagram.com/talktandem" class="icon instagram"><i class="fab fa-instagram"></i></a>
+      <a href="https://www.linkedin.com/talktandem" class="icon linkedin"><i class="fab fa-linkedin"></i></a>
+      <a href="mailto:contact@talktandem.com" class="icon email"><i class="far fa-envelope"></i></a>
+    </div>
+    <p class="copyright">© 2024 Talk Tandem. All rights reserved.</p>
+    </footer>
+</body>
+</html>
